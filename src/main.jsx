94f0e820c150bb2f2b74e64235cc21cb4a36d41d
francis leon <francisleon1186@yahoo.com>
2025-03-25 10:24:22 -0500

    actualizando datos installain npm i react-router-dom and prep of functions

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom' // esta supuestamente tiene que star qui debido a instalamos ya las dependencias npi react-router-dom  asegurarse de eso 
+
 import './index.css'
 import Home from './Home'
-import Login from './pages/Login'
+import react from '@vitejs/plugin-react-swc'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login/>
+    
     <Home/>
   </StrictMode>,
 )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,29 +4,29 @@ import './Login.css'
 function Login(){
         return( 
             
-            <div class="container">
+            <div className="container">
                 <input type="checkbox" id="signup_toggle"/>
-                <form class="form">
-                    <div class="form_front">
-                        <div class="form_details">Login</div>
-                        <input placeholder="Username" class="input" type="text"/>
-                        <input placeholder="Password" class="input" type="text"/>
-                        <button class="btn">Login</button>
-                        <span class="switch">Don't have an account? 
-                            <label class="signup_tog" for="signup_toggle">
+                <form className="form">
+                    <div className="form_front">
+                        <div className="form_details">Login</div>
+                        <input placeholder="Username" className="input" type="text"/>
+                        <input placeholder="Password" className="input" type="text"/>
+                        <button className="btn">Login</button>
+                        <span className="switch">Don't have an account? 
+                            <label className="signup_tog" for="signup_toggle">
                                 Sign Up
                             </label>
                         </span>
                     </div>
-                    <div class="form_back">
-                        <div class="form_details">SignUp</div>
-                        <input placeholder="Firstname" class="input" type="text"/>
-                        <input placeholder="Username" class="input" type="text"/>
-                        <input placeholder="Password" class="input" type="text"/>
-                        <input placeholder="Confirm Password" class="input" type="text"/>
-                        <button class="btn">Signup</button>
-                        <span class="switch">Already have an account? 
-                            <label class="signup_tog" for="signup_toggle">
+                    <div className="form_back">
+                        <div className="form_details">SignUp</div>
+                        <input placeholder="Firstname" className="input" type="text"/>
+                        <input placeholder="Username" className="input" type="text"/>
+                        <input placeholder="Password" className="input" type="text"/>
+                        <input placeholder="Confirm Password" className="input" type="text"/>
+                        <button className="btn">Signup</button>
+                        <span className="switch">Already have an account? 
+                            <label className="signup_tog" for="signup_toggle">
                                 Sign In
                             </label>
                         </span>
