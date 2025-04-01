74803519190717ec65c8fee3cac22783fed5856a
francis leon <francisleon1186@yahoo.com>
2025-04-01 10:27:03 -0500

    algunos otros cambios en la ruta y login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import './Login.css'
 
 
 function Login(){
+   const [getUser,setUser]=useState()// esta linea de codigo se hace por cada valor que ingreses por teclado.
+   const [getPassword,setPassword]=useState()// esta linea de codigo se hace por cada valor que ingreses por teclado.
+   const [getEmail,setEmail]=useState("correo")// esta linea de codigo se hace por cada valor que ingreses por teclado.
         return( 
-            
+            //analizando los datos de username y password repitiendo estados y actualizando dato....busccar username y password
             <div className="container">
                 <input type="checkbox" id="signup_toggle"/>
                 <form className="form">
@@ -11,6 +15,8 @@ function Login(){
                         <div className="form_details">Login</div>
                         <input placeholder="Username" className="input" type="text"/>
                         <input placeholder="Password" className="input" type="text"/>
+                        <input value={getEmail} placeholder="email" className="input" type="text"/> {/*aqui alteramos la info  y mirar los cambios en la redendarizacions*/}
+                       
                         <button className="btn">Login</button>
                         <span className="switch">Don't have an account? 
                             <label className="signup_tog" for="signup_toggle">
