0f21d9786ecec094957e9ec464f6561839234eae
francis leon <francisleon1186@yahoo.com>
2025-04-01 08:54:14 -0500

    analizando como funciona el enrutador

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import { createRoot } from 'react-dom/client'
 import {createBrowserRouter,RouterProvider} from 'react-router-dom' // esta supuestamente tiene que star qui debido a instalamos ya las dependencias npi react-router-dom  asegurarse de eso 
 
 import './index.css'
-import Home from './Home'
-import react from '@vitejs/plugin-react-swc'
+import { enrutador } from './router/enrutador'
+
+
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
-    <Home/>
+    <RouterProvider router={createBrowserRouter(enrutador)}/>
   </StrictMode>,
 )
---
+++ src/router/enrutador.jsx
@@ -0,0 +1,20 @@
+import Login from '../pages/Login'
+import Home from '../Home'
+export let enrutador =[
+    {
+        path:'/',// recuerda que aqui se necesita por lo menos tener un slash en el path dentro de los objetos 
+        element:<Login/>
+    },
+    {
+        path:'/home',
+        element:<Home/>
+
+    }
+
+]
+//export{
+ //   enrutador
+//}
+
+// estos son los tres tipos de exportacion 
+// export default enrutador as well
