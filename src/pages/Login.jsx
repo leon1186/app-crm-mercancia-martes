fba37721df16e903cab65fd7b7d4d5a83f82284e
francis leon <francisleon1186@yahoo.com>
2025-04-29 10:28:30 -0500

    implementacion del metodo POST

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { usuarios } from '../services/database'
+import { useEffect, useState } from 'react'
+//import { usuarios } from '../services/database'
 import './Login.css'
 import { alertaRedireccion,alertError,generarToken } from '../helpers/funciones'// nos ahorramos lineas de codigo.
 import { useNavigate } from 'react-router-dom'
@@ -8,13 +8,48 @@ import { useNavigate } from 'react-router-dom'
 
 //console.log(usuarios) para comprobar que los datos estan presentes en la web
 
+
+
+let apiUsuario='https://back-json-server-martes.onrender.com/usuarios'// esto es el back end
+
+
 function Login(){
-    console.log(generarToken());
+    //console.log(generarToken());
    const [getUser,setUser]=useState()// esta linea de codigo se hace por cada valor que ingreses por teclado.
    const [getPassword,setPassword]=useState()// esta linea de codigo se hace por cada valor que ingreses por teclado.el metodo get es para consultar a una base de datos.
    //const [getEmail,setEmail]=useState("correo")// esta linea de codigo se hace por cada valor que ingreses por teclado.
    // estas funciones creadas van a depender del componente login.
+   const[getName,setName]=useState("");
+   const[getEmail,setEmail]=useState(""); 
+
+
+   const [usuarios,setUsuarios]=useState([]);// muy importante entender esto OJO
+
    let redireccion=useNavigate()
+
+   function getUsuarios(){
+    //fetch(apiUsuario).then((response)=>console.log(response)) //esto es para mirara completamente la respuesta lo que nos
+    // interesa es el json donde viene el contenido
+    fetch(apiUsuario)
+        .then((response)=>response.json())
+        .then((data)=>setUsuarios(data))
+        .catch((error)=>console.log(error));
+        
+// se deberia imprimir dos veces por el mode strict en el main
+   }// RECUERDA QUE UNA FUNCION SE TIENE QUE INVOCAR.
+   
+   useEffect(()=>{getUsuarios();},[]);// del esta original de un objecto vacio , un estado por defecto
+   getUsuarios();
+   //console.log(usuarios);//yo desde el front hago una promes.pero asi va de manera infinita por eso llamas el useefect
+
+
+   
+   
+
+
+
+
+
    function buscarUsuario(){
       let usuarioEncontrado=usuarios.find((item)=>getUser==item.usuario && getPassword==item.contrasena)// item es una variable de control puedes llamarlo tambien index o la forma de iniciar sesion de un usuario o un correo pondriamos ("||") and && getTipousuario=item.tipoUsuario extraido de la base de datos.
       return usuarioEncontrado
@@ -32,6 +67,30 @@ function Login(){
     }
     }
     // si no le mando un mensaje especifico. siepre se redicionara en lo mismo
+
+
+
+    function registrarUsuario(){
+        let auth =usuarios.some((item)=>item.correo==getEmail || item.usuario==getUser)
+        if (auth){
+            alertError()
+        }else{
+            
+            let nuevoUsuario={
+                name:getName,
+                correo:getEmail,
+                usuario:getUser,
+                contrasena:getPassword,
+            }
+            
+            fetch(apiUsuario,{
+                method:"POST",
+                body:JSON.stringify()
+            })
+        }
+    }
+
+
     
    
    return( 
@@ -55,12 +114,12 @@ function Login(){
                     </div>
                     <div className="form_back">
                         <div className="form_details">SignUp</div>
-                        <input placeholder="Firstname" className="input" type="text"/>
-                        <input placeholder="Username" className="input" type="text"/>
-                        <input placeholder="Password" className="input" type="text"/>
-                        <input placeholder="Family " className="input" type="text"/>
-                        <input placeholder="Confirm Password" className="input" type="text"/>
-                        <button className="btn">Signup</button>
+                        <input placeholder="Firstname" className="input" type="text" onChange={(evtn)=>setFirstname(evtn.target.value)}/>
+                        <input placeholder="Username" className="input" type="text" onChange={(evtn)=>setUser(evtn.target.value)}/>
+                        <input placeholder="Password" className="input" type="text" onChange={(evtn)=>setPassword(evtn.target.value)}/>
+                        <input placeholder="email " className="input" type="text" onChange={(evtn)=>setEmail(evtn.target.value)}/>
+                        
+                        <button  type="button" onClick={registrarUsuario} className="btn">Signup</button>
                         <span className="switch">Already have an account? 
                             <label className="signup_tog" for="signup_toggle">
                                 Sign In
